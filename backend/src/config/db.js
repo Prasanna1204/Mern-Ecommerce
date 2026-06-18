ef7519c4df9d70cfae2f911ea1dba78d015c9a6f
AYYANAR PRASANNA B <baprasanna124@gmail.com>
2026-06-18 22:29:57 +0530

    mongodb connection and sever checking

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.mongodbURL);
+        console.log("MongoDB connected successfully");
+    }catch(error){
+        console.error("MongoDB connection failed", error);
+        process.exit(1);
+    }
+}
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,18 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import ConnectDB from "./config/db.js";
+dotenv.config();
+ConnectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
